8cb105933d5c1a5a39a872c344302f11fd584049
DEMENYACA <39350941+DEMENYACA@users.noreply.github.com>
2019-01-30 15:57:38 -0500

    Add files via upload

---
+++ Guess.js
@@ -0,0 +1,74 @@
+
+console.log ("Motivational Words");
+let i=hatred;
+while ('Love for all ___ for None...Khalifatul Masih III');
+alert (i)
+
+let answer=prompt ('Love for all ___ for None...Khalifatul Masih III');
+if (answer=="hatred") {
+    alert("Great! Stay Motivated!");
+}  else if (answer=="love") {
+    alert("Not quite, try again!");
+} else {
+    alert("Motivate Up!");
+}
+
+
+
+// let answer=prompt ('___ kills more dreams than failure ever will...Suze Kassem')
+// if (answer=="Doubt"){
+//  alert("Great! Stay Motivated!")
+// } else if(answer=="Fear"){
+//    alert ("Not quite, try again!")
+// } else {
+//     alert("Motivate Up!")
+// }
+// let answer=prompt('Die with ___not dreams...Unknown')
+// if(answer=="memories"){
+//     alert("Great! Stay Motivated!")
+// }else if (answer=="love"){
+//     alert("Not quite,try again! ")
+// }else {
+//     alert("Motivate Up!")
+// }
+// let answer=prompt("Everything you can ___is real! Pablo Picasso")
+// if(answer=="imagine"){
+//     alert ('Great! Stay Motivated!')
+// }else if(answer=="see"){
+//     alert("Not quite, try again!")
+// }else {
+//          alert("Motivate Up!")
+// }
+
+// let answer=prompt('___is the ulimate sophistication...Leanardo da Vinci')
+// if(answer=="Simplicity"){
+//     alert("Great! Stay Motivated!")
+// }else if (answer=="art"){
+//     alert("Not quite, try again!")
+// }else{
+//     alert("Motivate Up")
+// }
+
+
+// let answer=prompt('What we ____we become...Buddha')
+// if (answer=="think"){
+//    alert ("Great! Stay Motivated!")   
+// }else if (answer=="love"){
+//    alert ("Not quite, try again!")
+// }else{
+//    alert ("Motivate Up!")
+// }
+ 
+//  else if(answer==like){
+// }alert('No quite, try again!');
+//          else{(answer==hate)
+//              alert('Better get motivated!');
+//          }   
+//         }
+//     }
+
+//  let i = 25;
+//  while (i<29){ // shows 25, then 26, then 27,then 28
+//     alert(i); 
+//     i++;  
+// }
