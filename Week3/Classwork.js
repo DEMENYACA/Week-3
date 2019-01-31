8daf29dc4032e3b032ff16e235dc37a7363c3c4e
DEMENYACA <39350941+DEMENYACA@users.noreply.github.com>
2019-01-31 16:07:17 -0500

    Thursday's class stuff

---
+++ Week3/Classwork.js
@@ -0,0 +1,6 @@
+//  let i = 25;
+//  while (i<29){ // shows 25, then 26, then 27,then 28
+//     alert(i); 
+//     i++;  
+// }
+
---
+++ Week3/Guess.js
@@ -0,0 +1,80 @@
+
+console.log ("Motivational Words");
+
+
+let answer="";
+while(answer!="hatred"){
+answer=prompt ('Love for all ___ for none...Khalifatul Masih III');
+if (answer=="hatred") {
+    alert("Great! Stay Motivated!");
+}  else if (answer=="") { 
+    // if else isn't working!!!!!!!!!!
+    alert("Not quite, try again!"); 
+} else {
+    alert("Keep your head up!");
+}
+}
+
+
+
+
+
+
+let answer=prompt ('___ kills more dreams than failure ever will...Suze Kassem')
+if (answer=="Doubt"){
+ alert("Great! Stay Motivated!")
+} else if(answer=="Fear"){
+   alert ("Not quite, try again!")
+} else {
+    alert("Motivate Up!")
+ 
+}
+let answer=prompt('Die with ___not dreams...Unknown')
+if(answer=="memories"){
+    alert("Great! Stay Motivated!")
+}else if (answer=="love"){
+    alert("Not quite,try again! ")
+}else {
+    alert("Motivate Up!")
+}
+let answer=prompt("Everything you can ___is real! Pablo Picasso")
+if(answer=="imagine"){
+    alert ('Great! Stay Motivated!')
+}else if(answer=="see"){
+    alert("Not quite, try again!")
+}else {
+         alert("Motivate Up!")
+}
+
+let answer=prompt('___is the ulimate sophistication...Leanardo da Vinci')
+if(answer=="Simplicity"){
+    alert("Great! Stay Motivated!")
+}else if (answer=="art"){
+    alert("Not quite, try again!")
+}else{
+    alert("Motivate Up")
+}
+
+
+let answer=prompt('What we ____we become...Buddha')
+if (answer=="think"){
+   alert ("Great! Stay Motivated!")   
+}else if (answer=="love"){
+   alert ("Not quite, try again!")
+}else{
+   alert ("Motivate Up!")
+}
+ 
+let answer=prompt('Yesterday you said___...Nike.')
+if(answer=="Just do it!"){
+    alert("Great! Stay Motivated!")
+}else if (answer=="tommorow"){
+
+    alert("No quite, try again!")
+}else{
+    alert("Better get motivated!")
+}   
+
+
+
+
